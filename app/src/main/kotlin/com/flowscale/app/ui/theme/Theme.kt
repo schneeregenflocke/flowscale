18d45018f444f73a2fe6c8825b418be88d1f3deb
schneeregenflocke <schneeregenflocke@gmail.com>
2026-04-01 11:03:21 +0200

    Revert "Define color schemes in Theme.kt and create Color.kt for better theme management"
    This reverts commit 36f74810a0fc900e5ca1b38a1ec0763c44b4100e.

--- app/src/main/kotlin/com/flowscale/app/ui/theme/Theme.kt
+++ app/src/main/kotlin/com/flowscale/app/ui/theme/Theme.kt
@@ -10,17 +10,8 @@ import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
-private val LightColorScheme = lightColorScheme(
-    primary = Green40,
-    secondary = GreenGrey40,
-    tertiary = Teal40,
-)
-
-private val DarkColorScheme = darkColorScheme(
-    primary = Green80,
-    secondary = GreenGrey80,
-    tertiary = Teal80,
-)
+private val LightColorScheme = lightColorScheme()
+private val DarkColorScheme = darkColorScheme()
 
 @Composable
 fun FlowScaleTheme(
