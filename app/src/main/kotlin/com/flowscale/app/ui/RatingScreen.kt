7411cc26b7d8a79d4fb9166d26c6fee99dec435c
Marco Peyer <schneeregenflocke@gmail.com>
2026-04-16 18:30:34 +0200

    feat: add navigation bars padding to RatingScreen button

--- app/src/main/kotlin/com/flowscale/app/ui/RatingScreen.kt
+++ app/src/main/kotlin/com/flowscale/app/ui/RatingScreen.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
@@ -206,6 +207,7 @@ fun RatingScreen(viewModel: RatingViewModel, modifier: Modifier = Modifier) {
             onClick = { showAbout = true },
             modifier = Modifier
                 .align(Alignment.BottomEnd)
+                .navigationBarsPadding()
                 .padding(16.dp),
         ) {
             Icon(
